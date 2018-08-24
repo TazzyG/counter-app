ffb27a8b78b36ff7e5b896a9c84c01fedb9691f8
Laurie Gloge <laurie@friendlyroad.com>
2018-08-24 12:58:27 -0600

    remove local state in counter.jsx for controlled component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,20 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
+  // state = {
+  //   value: this.props.counter.value
+  // };
 
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
-
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     var product = { id: 1 };
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement(product)}
+          onClick={() => thi.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           {" "}
@@ -37,12 +31,12 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }

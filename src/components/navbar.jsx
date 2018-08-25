4e2a5ea790a14235ac13491952b24a319b185103
Laurie Gloge <laurie@friendlyroad.com>
2018-08-24 18:08:02 -0600

    lifting counter class to use in nav, adding stateless component to navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+// Stateless functional Component sfc
+const NavBar = props => {
+  class NavBar extends Component {
+    render() {
+      return (
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              {props.totalCounters}
+            </span>
+          </a>
+        </nav>
+      );
+    }
+  }
+};
+
+export default NavBar;
